fb27fb6ef98f7349e52b2379b69921a149289d2d
Thfirmin <thfirmin@student.42.rio>
2024-03-30 16:37:08 -0300

    update

--- workspace/srcs/requirements/nginx/tools/index.sh
+++ workspace/srcs/requirements/nginx/tools/index.sh
@@ -2,10 +2,14 @@
 
 echo "Hello, World! Nginx container NEW"
 
+echo "pwd: $(pwd) | app-vol: $(ls /app-vol) | volume: $(ls /)"
+
 if [ -f '/app-vol/readme.txt' ]; then
-	echo "It's already have a readme.txt here XD"
+	echo "It's already have a readme.txt here XDsdfsdfsdf"
 	cat /app-vol/readme.txt
+	echo "Thino is heeere" >> /app-vol/readme.txt
 else
-	echo "Isn't have a readme.txt here, i'll write one for ü ;)"
+	echo "Isn't have a readme.txt here, i'll write one for ü ;)ssdas"
+	ls /app-vol
 	echo -e "# README.TXT\n\nI'm a readme.txt XD" > /app-vol/readme.txt
 fi
---
+++ workspace/srcs/requirements/nginx/volume/readme.txt
@@ -0,0 +1,5 @@
+# README.TXT
+
+Hi! I'm readme.txt XD
+
+FUCK YOU!!!
