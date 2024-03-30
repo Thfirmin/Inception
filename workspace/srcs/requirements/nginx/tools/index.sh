5a773f634ab2ceea74ca18af9a214969429e28cf
Thfirmin <thfirmin.dsantos+github@gmail.com>
2024-03-30 15:01:19 -0300

    update

--- workspace/srcs/requirements/nginx/tools/index.sh
+++ workspace/srcs/requirements/nginx/tools/index.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "Hello, World! Nginx container NEW"
+
+if [ -f '/app-vol/readme.txt' ]; then
+	echo "It's already have a readme.txt here XD"
+	cat /app-vol/readme.txt
+else
+	echo "Isn't have a readme.txt here, i'll write one for ü ;)"
+	echo -e "# README.TXT\n\nI'm a readme.txt XD" > /app-vol/readme.txt
+fi
