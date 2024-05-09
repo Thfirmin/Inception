b97a9dd58bb01b233048108d5ca7d9f1f217e10f
Thfirmin <thfirmin@student.42.rio>
2024-05-08 21:29:11 -0300

    update

--- workspace/srcs/requirements/mariadb/tools/index.sh
+++ workspace/srcs/requirements/mariadb/tools/index.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Hello, $1!"
+chown -R mysql:mysql ${DB_VOLUME_PATH}
+chmod -R 777 ${DB_VOLUME_PATH}
+
+service mariadb start
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+mysql -e "FLUSH PRIVILEGES;"
+
+kill  $(cat /var/run/mysqld/mysqld.pid)
+
+mariadbd
